7a6726e70be7ddb8afb4641deced83e3be5983bc
Tasos Gretsistas <shadowthehedgehogu@msn.com>
2021-08-18 01:37:17 +0300

    Records are cool

--- cs/RetSim/RetSim/Race.cs
+++ cs/RetSim/RetSim/Race.cs
@@ -1,76 +1,17 @@
-﻿namespace RetSim
+﻿using System.Collections.Generic;
+
+namespace RetSim
 {
-    public record Race
-    {
-        public string Name { get; set; } = "Fictional Race";
-        public BaseStats Stats { get; set; }
-
-        public Race()
-        {
-            Name = "Fictional Race";
-            Stats = new BaseStats();
-        }
-
-        public Race(string name, BaseStats stats)
-        {
-            Name = name;
-            Stats = stats;
-        }
-    }
+    public record Race(string Name, RaceStats Stats);
+    public record RaceStats(int Strength, int Agility, int Intellect, int Stamina, int Health, int Mana, int AttackPower, float CritChance, float SpellCritChance, int Expertise);
 
     public static class Races
     {
-        public static Race Human = new Race("Human", new BaseStats(126, 77, 83, 120, 3197, 2673, 190, 0.6f, 3.32f, 5));
-    }
+        public static Race Human = new Race("Human", new RaceStats(126, 77, 83, 120, 3197, 2673, 190, 0.6f, 3.32f, 5));
 
-    public record BaseStats
-    {
-        public int Strength { get; set; }
-        public int Agility { get; set; }
-        public int Intellect { get; set; }
-        public int Stamina { get; set; }
-
-        public int Health { get; set; }
-        public int Mana { get; set; }
-
-        public int AttackPower { get; set; }
-        public float CritChance { get; set; }
-        public int Expertise { get; set; }
-
-        public float SpellCritChance { get; set; }
-
-        public BaseStats()
+        public static Dictionary<string, Race> ByName = new Dictionary<string, Race>()
         {
-            Strength = 1;
-            Agility = 1;
-            Intellect = 1;
-            Stamina = 1;
-
-            Health = 1;
-            Mana = 1;
-
-            AttackPower = 1;
-            CritChance = 0;
-            Expertise = 0;
-
-            SpellCritChance = 0;
-        }
-
-        public BaseStats(int bStrength, int bAgility, int bIntellect, int bStamina, int bHealth, int bMana, int bAttackPower, float bCritChance, float bSpellCritChance, int bExpertise = 0)
-        {
-            Strength = bStrength;
-            Agility = bAgility;
-            Intellect = bIntellect;
-            Stamina = bStamina;
-
-            Health = bHealth;
-            Mana = bMana;
-
-            AttackPower = bAttackPower;
-            CritChance = bCritChance;
-            Expertise = bExpertise;
-
-            SpellCritChance = bSpellCritChance;
-        }
-    }
+            { Human.Name, Human }
+        };
+    }    
 }
