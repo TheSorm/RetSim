26db075fc5bcf24aca26b0e66eac3aeb52b0471d
TheSorm <thesorm@web.de>
2021-08-18 12:54:10 +0200

    Added a logger that dosnt log

---
+++ cs/RetSim/RetSim/Loggers/NonLogger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetSim.Loggers
+{
+    class NonLogger : AbstractLogger
+    {
+        public override void Log(string message)
+        {}
+    }
+}
