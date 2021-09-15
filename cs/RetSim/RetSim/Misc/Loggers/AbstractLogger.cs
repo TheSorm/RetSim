7a0c015b078c8f9b8f257aa349fb2c7fcce9ebc3
TheSorm <thesorm@web.de>
2021-09-15 12:10:47 +0200

    cleaned up pages

--- cs/RetSim/RetSim/Misc/Loggers/AbstractLogger.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RetSim.Misc.Loggers;
-
-public abstract class AbstractLogger
-{
-    public abstract void Log(string message);
-
-    public abstract void DisableInput();
-    public abstract void EnableInput();
-}
