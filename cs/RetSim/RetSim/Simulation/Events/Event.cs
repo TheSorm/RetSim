2d131d287b9dcab09ad0a6449ba4e9afe61304cc
TheSorm <thesorm@web.de>
2021-09-17 13:37:37 +0200

    Enhanced Event comparison

--- cs/RetSim/RetSim/Simulation/Events/Event.cs
+++ cs/RetSim/RetSim/Simulation/Events/Event.cs
@@ -34,9 +34,9 @@ public abstract class Event : IComparable<Event>
 
     public int CompareTo(Event other)
     {
-        int result = Timestamp.CompareTo(other.Timestamp);
+        int result = Timestamp - other.Timestamp;
         if (result == 0) 
-            return Priority.CompareTo(other.Priority);
+            return Priority - other.Priority;
         return result;
     }
 }
