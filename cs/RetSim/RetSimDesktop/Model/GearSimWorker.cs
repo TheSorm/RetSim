61338866608ba5f32232ce8d11b26db4d0db91df
Tasos Gretsistas <shadowthehedgehogu@msn.com>
2021-11-12 18:15:05 +0200

    Fixed error

--- cs/RetSim/RetSimDesktop/Model/GearSimWorker.cs
+++ cs/RetSim/RetSimDesktop/Model/GearSimWorker.cs
@@ -37,6 +37,7 @@ namespace RetSimDesktop.View
                     var talents = SelectedTalents.GetTalentList(input.Item1);
                     var buffs = Spell.GetSpells(25580, 27141, 25898, 2048, 25528, 25359, 25570, 30811, 26991, 25392, 32999, 27127, 28878);
                     var debuffs = Spell.GetSpells(20337, 14169, 33602, 27226, 30070, 32484, 17800, 33200, 15258, 22959, 14325, 34501);
+                    var consumables = Spell.GetSpells(28520, 33256, 33082, 33077, 35476, 23060);
 
                     float overallDPS = 0;
                     for (int i = 0; i < numberOfSimulations; i++)
@@ -44,7 +45,7 @@ namespace RetSimDesktop.View
                         FightSimulation fight = new(
                             new Player("Brave Hero", RetSim.Data.Collections.Races["Human"], playerEquipment, talents), 
                             new Enemy("Magtheridon", CreatureType.Demon, ArmorCategory.Warrior), 
-                            new EliteTactic(), buffs, debuffs, input.Item1.SimSettings.MinFightDuration, input.Item1.SimSettings.MaxFightDuration);
+                            new EliteTactic(), buffs, debuffs, consumables, input.Item1.SimSettings.MinFightDuration, input.Item1.SimSettings.MaxFightDuration);
                         fight.Run();
                         overallDPS += fight.CombatLog.DPS;
                         if (i % (numberOfSimulations / 100 + 1) == 0)
